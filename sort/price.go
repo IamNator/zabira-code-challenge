368ac320ee1abba3b3ba9732df77a7bdaaab5d86
Nator Verinumbe <natverior1@gmail.com>
2023-04-12 15:16:19 +0000

    optimized sort -> checking boolean only once

--- sort/price.go
+++ sort/price.go
@@ -15,10 +15,24 @@ type PriceSorter struct{} // Sorts products by price
 // Bool is used instead of int to make the interface more readable
 // and to avoid any confusion about the meaning of the int or string value (e.g. 0 = ascending, 1 = descending) or ("asc" = ascending [ASC, aSc ...], "desc" = descending [DESC, dEsc ...]])
 func (ps *PriceSorter) Sort(products []model.Product, desc bool) {
+
+	//
+	// There is duplication here, but I think it's better to have the code duplicated
+	//
+	// My reasoning is that every time the loop is executed, the compiler has to check if the condition is true
+	//
+	// Doing it this way, the compiler can optimize the code and only check the condition once
+	//
+
+	if desc {
+		sort.Slice(products, func(i, j int) bool {
+			return products[i].Price > products[j].Price // previous price > current price [descending]
+		})
+		return
+	}
+
 	sort.Slice(products, func(i, j int) bool {
-		if desc {
-			return products[i].Price > products[j].Price
-		}
-		return products[i].Price < products[j].Price
+		return products[i].Price < products[j].Price // previous price < current price [ascending]
 	})
+
 }
--- sort/sales_view.go
+++ sort/sales_view.go
@@ -15,14 +15,34 @@ type SalesToViewRatioSorter struct{} // Sorts products by sales to view ratio
 // Bool is used instead of int to make the interface more readable
 // and to avoid any confusion about the meaning of the int or string value (e.g. 0 = ascending, 1 = descending) or ("asc" = ascending [ASC, aSc ...], "desc" = descending [DESC, dEsc ...]])
 func (svrs *SalesToViewRatioSorter) Sort(products []model.Product, desc bool) {
+
+	//
+	// There is duplication here, but I think it's better to have the code duplicated
+	//
+	// My reasoning is that every time the loop is executed, the compiler has to check if the condition is true
+	//
+	// Doing it this way, the compiler can optimize the code and only check the condition once
+	//
+
+	if desc {
+
+		sort.Slice(products, func(i, j int) bool {
+
+			salesToViewRatioI := float64(products[i].SalesCount) / float64(products[i].ViewsCount)
+			salesToViewRatioJ := float64(products[j].SalesCount) / float64(products[j].ViewsCount)
+
+			return salesToViewRatioI > salesToViewRatioJ // previous sales to view ratio > current sales to view ratio [descending]
+
+		})
+
+		return
+	}
+
 	sort.Slice(products, func(i, j int) bool {
 
 		salesToViewRatioI := float64(products[i].SalesCount) / float64(products[i].ViewsCount)
 		salesToViewRatioJ := float64(products[j].SalesCount) / float64(products[j].ViewsCount)
 
-		if desc {
-			return salesToViewRatioI > salesToViewRatioJ
-		}
-		return salesToViewRatioI < salesToViewRatioJ
+		return salesToViewRatioI < salesToViewRatioJ // previous sales to view ratio < current sales to view ratio [ascending]
 	})
 }
